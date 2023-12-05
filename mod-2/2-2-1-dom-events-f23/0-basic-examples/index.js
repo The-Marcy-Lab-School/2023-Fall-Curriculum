79a63ec8f205b05cc43f58f549a38b5c5ee5e7f6
benspector-mls <ben@marcylabschool.org>
2023-12-05 09:28:46 -0500

    adding events code

---
+++ mod-2/2-2-1-dom-events-f23/0-basic-examples/index.js
@@ -0,0 +1,11 @@
+// a generic event handler
+const eventHandler = (event) => console.log(event);
+
+// register an event listener on a button
+const button = document.querySelector('button')
+const mouseArea = document.querySelector('#mouse-area');
+
+// TODO:
+// add a 'click' handler for the button
+// add a 'mousemove' handler for the mouseArea
+// add a 'keydown' handler for the entire document
